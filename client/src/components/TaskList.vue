eefc590ccbdab3f2e3fee0a5fd5aab987943f642
thomas-almeida <thomas.almeida@tradeupgroup.com>
2023-11-27 19:55:07 -0300

    First styles

--- client/src/components/TaskList.vue
+++ client/src/components/TaskList.vue
@@ -1,11 +1,15 @@
 <template>
-    <v-list lines="two">
+    <v-list lines="two" class="flex">
       <v-list-item
         v-for="task in tasks"
         :key="task.id"
         :title="task.title"
         :subtitle="task.details"
+        class="task-item"
       >
+      <v-btn icon="mdi-delete" variant="plain"></v-btn>
+      <v-btn icon="mdi-pencil" variant="plain"></v-btn>
+      <v-btn icon="mdi-check-circle" variant="plain"></v-btn>
       </v-list-item>
     </v-list>
   </template>
@@ -24,3 +28,29 @@
 
   </script>
   
+  <style>
+
+    .flex {
+      display: flex;
+      justify-content: start;
+      align-items: center;
+      flex-wrap: wrap;
+      gap: 20px;
+      padding: 20px;
+      margin: 10px;
+    }
+
+    .task-item {
+      border: 1px solid #eee;
+      border-radius: 5px;
+      box-shadow: 1px 2px 4px #d5d5d5;
+      width: 30%;
+      transition: 0.2s;
+    }
+
+    .task-item:hover {
+      transform: scale(1.06);
+      cursor: pointer;
+      box-shadow: 1px 2px 4px #939393;
+    }
+  </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
-  <CreateModal />
-  <TaskList />
+  <div class="container">
+    <div class="container-content">
+      <CreateModal />
+      <TaskList />
+    </div>
+  </div>
 </template>
 
 <script setup>
 import CreateModal from '@/components/CreateModal.vue';
 import TaskList from '@/components/TaskList.vue'
 </script>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+  }
+
+  .container-content {
+    width: 70%;
+  }
+
+</style>
